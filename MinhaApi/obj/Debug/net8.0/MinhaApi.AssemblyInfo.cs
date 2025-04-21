1786e8385b1dea113c44a90ee466e056ad060c16
Andre Urpia Rajo Jr <dreurpia@gmail.com>
2025-04-21 07:38:01 -0300

    setup: update version .Net

--- MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
+++ MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95888194ae4af21e875fd26c5aea5d7f786ba80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
